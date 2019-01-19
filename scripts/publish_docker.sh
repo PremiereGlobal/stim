02c317e3b726d21a3988c623e8d19f8ec70b3671
Chris Bartlett <bartlettc@gmail.com>
2019-01-18 17:09:07 -0700

    Adding travis build

---
+++ scripts/publish_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+SOURCE_VERSION=${1:-master}
+PUBLISH_VERSION=${2:-$SOURCE_VERSION}
+DOCKER_REPO="readytalk/stim"
+
+docker tag ${DOCKER_REPO}:${SOURCE_VERSION}-linux ${DOCKER_REPO}:${PUBLISH_VERSION}
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker push ${DOCKER_REPO}:${PUBLISH_VERSION}
