0e131f0af97721c831dab24790507624de169974
Chris Bartlett <bartlettc@gmail.com>
2019-01-29 16:43:08 -0700

    Fixing installer update

--- scripts/update_installer.sh
+++ scripts/update_installer.sh
@@ -8,5 +8,5 @@ cd bin
 sed -i 's/^VERSION\=v.*$/VERSION\='${VERSION}'/g' install.sh
 SHA_DARWIN=$(sha256sum stim-darwin-${VERSION}.tar.gz | cut -d' ' -f 1)
 sed -i 's/^SHA_DARWIN\=.*$/SHA_DARWIN\='${SHA_DARWIN}'/g' install.sh
-SHA_LINUX=$(sha256sum stim-darwin-${VERSION}.tar.gz | cut -d' ' -f 1)
+SHA_LINUX=$(sha256sum stim-linux-${VERSION}.tar.gz | cut -d' ' -f 1)
 sed -i 's/^SHA_LINUX\=.*$/SHA_LINUX\='${SHA_LINUX}'/g' install.sh
