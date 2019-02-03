f5f90d2dcdcb6fb8a695a4ca6a598c869871d61d
Ryan W <ryanw@thorix.net>
2019-02-02 23:28:24 -0700

    adding golang.org comments

--- stim/stim.go
+++ stim/stim.go
@@ -87,6 +87,10 @@ func (stim *Stim) Pagerduty() *pagerduty.Pagerduty {
 	return pagerduty
 }
 
+// Vault is the interface for Hashicorp Vault wrapper methods
+// The main input is the vault-address
+// Will prompt the user for their LDAP username and password
+// Will update the user's ~/.vault-token file with a new token
 func (stim *Stim) Vault() *vault.Vault {
 
 	if stim.vault == nil {
@@ -103,7 +107,7 @@ func (stim *Stim) Vault() *vault.Vault {
 		}
 
 		vault, err := vault.New(&vault.Config{
-			Address:  stim.GetConfig("vault-address"),
+			Address:  stim.GetConfig("vault-address"), // Default is 127.0.0.1
 			Noprompt: stim.GetConfigBool("noprompt") == false && stim.IsAutomated(),
 			Logger:   stim.log,
 			Username: username,
@@ -112,7 +116,7 @@ func (stim *Stim) Vault() *vault.Vault {
 			stim.log.Fatal("Stim-Vault: Error Initializaing: ", err)
 		}
 
-		// Update the user set in local configs to make new logins friendly
+		// Update the username set in local configs to make any new logins friendly
 		err = stim.UpdateVaultUser(vault.GetUser())
 		if err != nil {
 			stim.log.Fatal("Stim-Vault: Error Updating username in configuration file: ", err)
@@ -137,6 +141,8 @@ func (stim *Stim) User() (string, error) {
 	return user.Username, nil
 }
 
+// UpdateVaultUser updates the user's stim config file with given username
+// This username will be the default option when authenticating against Vault
 func (stim *Stim) UpdateVaultUser(username string) error {
 	if username != stim.GetConfig("vault-username") {
 		stim.Set("vault-username", username)
@@ -149,6 +155,8 @@ func (stim *Stim) UpdateVaultUser(username string) error {
 	return nil
 }
 
+// IsAutomated simply guesses if a build is invoking this code
+// Used to disable user input prompts
 func (stim *Stim) IsAutomated() bool {
 	if os.Getenv("JENKINS_URL") == "" {
 		return false
