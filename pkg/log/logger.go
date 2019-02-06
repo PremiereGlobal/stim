49259049baa09e70a3077c6545cdc53bbc92e5ea
Ryan W <ryanw@thorix.net>
2019-02-06 00:20:07 -0700

    Added some notes around logging

--- pkg/log/logger.go
+++ pkg/log/logger.go
@@ -1,5 +1,8 @@
 package log
 
+// Logging interface to create standard calls like: log.Debug("Woot")
+// Avoid all other packages from declaring their own loggers
+// This strategy enables simple change of the backend logger
 type Logger interface {
 	Debug(...interface{})
 	Warn(...interface{})
@@ -8,6 +11,9 @@ type Logger interface {
 
 var logger Logger
 
+// Pass your logger object to this function
+// After the logger is setup it will be available across your packages
+// If SetLogger is not used Debug will not create output
 func SetLogger(givenLogger Logger) {
 	logger = givenLogger
 }
