ba8adc1a95e5465da7b1a040300704fad0c0a8a0
Chris Bartlett <bartlettc@gmail.com>
2019-07-18 13:59:13 -0600

    fixing typo

--- stim/config.go
+++ stim/config.go
@@ -128,7 +128,7 @@ func (stim *Stim) loadConfigFile() error {
 	configFile := stim.GetConfig("config-file")
 	_, err := os.Stat(configFile)
 	if err != nil && !os.IsExist(err) {
-		stim.log.Warn("No config file exits at :\"" + configFile + "\"")
+		stim.log.Warn("No config file exists at :\"" + configFile + "\"")
 		//If they passed in a custom path we might want to exit here
 	}
 	stim.config.SetConfigFile(configFile)
