1946accb200e366ace946ba9cb91bb96b518ca89
Eric Sherman <eric.sherman@pgi.com>
2021-03-17 02:34:28 -0400

    fixes an edge case in vault.Filter
    Vault's `CapabilitiesSelf` returns a "path" called `capabilities` when
    it receives only a single path in its request.  Presumably this is for
    backward compatibility.  It also returns the path with its capabilities,
    as it would when it returns a list of paths with their capabilities,
    which is what consumers of `vault.Filter` expect.
    
    So we just throw away the `capabilities` "path".  Otherwise users would
    see a dummy `capabilities` role along with the single expected role on
    `aws login` with `--filter-by-token` when the aws account only has a
    single role defined, or if their service account filter via `--saf`
    narrowed the set of possible roles down to 1.

--- pkg/vault/filter.go
+++ pkg/vault/filter.go
@@ -25,6 +25,9 @@ func (v *Vault) Filter(paths []string, withCapabilities []string) ([]string, err
 	var filteredPaths []string
 	for path, capabilities := range results.Data {
 		for _, capability := range withCapabilities {
+			if path == "capabilities" {
+				continue
+			}
 			if utils.Contains(capabilities, capability) {
 				filteredPaths = append(filteredPaths, path)
 				break
